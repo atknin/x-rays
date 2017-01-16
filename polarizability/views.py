d4e75393e8ceacb3c323abd7d9f8205fb48dad29
atknin <atknini@yahoo.com>
2017-01-16 08:43:04 +0300

    2017-01-16 05:43:04

--- polarizability/views.py
+++ polarizability/views.py
@@ -35,7 +35,7 @@ def add_crystal(request):
 				new.save()
 				# for i in request.POST['geom']:
 				# 	a = str(i[0])+str(i[1])+str(i[2])+str(i[3])+str(i[4])
-				bot_inform.sent_to_atknin_bot(request.POST['geom'], 'v') 
+				bot_inform.sent_to_atknin_bot(str(request.POST['geom']), 'v') 
 
 				message['status'] = "Успешно добавлено в базу"
 			else:
