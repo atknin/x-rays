840b3f3580f9f6e43c1dbc33a97ad0aa548fc145
atknin <atknini@yahoo.com>
2017-01-16 08:20:01 +0300

    2017-01-16 05:20:01

--- polarizability/views.py
+++ polarizability/views.py
@@ -21,7 +21,7 @@ def add_crystal(request):
 		try:
 			name = str(request.POST['id_name'])
 			bot_inform.sent_to_atknin_bot(name, 'v') # проинформируем в telegramm bot
-			if not polarizability_models.crystals.objects.objects.filter(name=name).exists():	
+			if not polarizability_models.crystals.objects.filter(name=name).exists():	
 				# polarizability_models.crystals.objects.create()пунь
 				message['status'] = "Успешно добавлено в базу"
 			else:
