4765fcb2ce9e6ed4e1fa945a6f2dc33f027a54d7
atknin <atknini@yahoo.com>
2017-01-18 12:00:53 +0300

    2017-01-18 09:00:53

--- general/views.py
+++ general/views.py
@@ -2,13 +2,26 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from general import models as general_models
-
+from django.contrib import auth
 
 # Create your views here.
 def index(request):
- 	return render(
-	 	request, 'general/index.html',
-	 	)
+	if request.is_ajax():
+		message = {}
+		login = request.POST['login']
+		password = request.POST['password']
+		user = auth.authenticate(username=login, password=password)
+		
+		if user is not None:
+			message['status'] = "ok"
+			auth.login(request, user)
+		else:
+			message['status'] = "error"
+		return JsonResponse(message)
+	else:
+	 	return render(
+		 	request, 'general/index.html',
+		 	)
 
 def about(request):
  	return render(
