dedf3aa7397b2c7fe6e43a1440bb24249dd169c1
atknin <atknini@yahoo.com>
2017-01-22 21:16:56 +0300

    2017-01-22 18:16:55

--- static/diffraction/js/zero_crystal.js
+++ static/diffraction/js/zero_crystal.js
@@ -4,3 +4,21 @@ $('#btn_0_cryst').click(function(event) {
 	$('#div_zero_crystal').show();
 
 });
+
+var canvas = new fabric.Canvas('fabric');
+canvas.setBackgroundImage("{% static 'diffraction/img/background.png' %}", canvas.renderAll.bind(canvas));
+
+var crystal1 = new fabric.Rect({
+  width: 30,
+  height: 30,
+  left: 570,
+  top: 135,
+  stroke: '#aaf',
+  strokeWidth: 1,
+  fill: '#FAFAFA',
+  selectable: false,
+  name: 'на это место можно установить кристалл',
+  class: 'place_sample1'
+});
+crystal1.scale(0.5)
+canvas.add(crystal1);
