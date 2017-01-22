3d696a4207f9505ee48505eb7b2aa2b21235a1e9
atknin <atknini@yahoo.com>
2017-01-22 21:44:22 +0300

    2017-01-22 18:44:21

--- static/diffraction/js/zero_crystal.js
+++ static/diffraction/js/zero_crystal.js
@@ -7,20 +7,7 @@ $('#btn_0_cryst').click(function(event) {
 
 var canvas = new fabric.Canvas('fabric');
 
-var crystal1 = new fabric.Rect({
-  width: 30,
-  height: 30,
-  left: 10,
-  top: 10,
-  stroke: '#aaf',
-  strokeWidth: 1,
-  fill: '#eee',
-  selectable: false,
-  name: 'на это место можно установить кристалл',
-  class: 'place_sample1'
-});
-crystal1.scale(0.5)
-canvas.add(crystal1);
+//--------- добавить источник --------------
 
 var source = new fabric.Image(document.getElementById('source'), {
   left: 650,
@@ -32,6 +19,22 @@ var source = new fabric.Image(document.getElementById('source'), {
   name: 'источник, нажмите для настройки',
   class: 'source_settings'
 });
-
 source.scale(0.5);
 canvas.add(source);
+
+// --------- добавть детектор ---------------
+var detector = new fabric.Image(document.getElementById('detector'), {
+  left: 250,
+  top: 135,
+  // angle: 30,
+  width: 100,
+  height: 30,
+  opacity: 0.85,
+  shadow: 'rgba(0,0,0,0.3) 5px 5px 5px',
+  selectable: false,
+  name: 'детектор, нажмите для настройки',
+  class: 'detector_settings'
+
+});
+detector.scale(0.5);
+canvas.add(detector);
