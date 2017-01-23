d51f04cbcbe7b8d696df0f821cc4e01765571234
atknin <atknini@yahoo.com>
2017-01-23 10:51:54 +0300

    2017-01-23 07:51:54

--- static/diffraction/js/zero_crystal.js
+++ static/diffraction/js/zero_crystal.js
@@ -94,14 +94,14 @@ check_array['input_l_slit1':false,'input_l_slit2':false,'input_size_slit1':false
 			'source_to_backend':false,];
 
 $('#input_l_slit1, #input_l_slit2').keyup(function() {
-  if($(this) == 'null'){
-  	alert('пусто');
+  if($(this).val() < 2 || $(this).val() > 0){
+  	alert('ok');
   };
 });
 
 $('#input_size_slit1, #input_size_slit2').keyup(function() {
-  if($(this) == 'null'){
-  	alert('пусто');
+  if($(this).val() < 2 || $(this).val() > 0){
+  	alert('ok');
   };
 });
 
