362e4bad837e282a8e714e64a078ead2e4b23e50
atknin <atknini@yahoo.com>
2017-01-30 20:30:38 +0300

    2017-01-30 17:30:34

--- general/views.py
+++ general/views.py
@@ -51,27 +51,44 @@ def converter(request):
 	if request.is_ajax():
 		message = {}
 		message['status'] = 'ok'
-		# input_data = {}
-		# with open('/home/atknin/env/xrays/media/'+ name +'.dat', 'w') as out:
-		# 	out.write('%14.8s' % 'keys')
-		# 	for key in request.POST:
-		# 		scan = request.POST[key].split('//')
-		# 		input_data[key] = scan
-		# 		out.write('%14.8s' % key)
-		# 	out.write('\n')
-		# 	del input_data['csrfmiddlewaretoken']
-		# 	valid_letters='ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-		# 	name = ''.join((random.choice(valid_letters) for i in range(5)))
-		# 	for i in range(len(input_data['scan1'][0].split()))
-		# 		out.write('%14.8s' % input_data[key][0].split())
-		# 		for key in input_data:
-		# 			input_data[key]
-		# 			out.write('%14.8s' % key)
+		input_data = {}
+		valid_letters='ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+		data = []
+		name = ''.join((random.choice(valid_letters) for i in range(5)))
+		with open('/home/atknin/env/xrays/media/'+ name +'.dat', 'w') as out:
+			out.write('%14.8s' % 'keys')
+			j = 0
+			for key in request.POST:
+				input_q = request.POST[key].split('//')
+				data['x' + str(j)] = input_q[0].split()
+				data['a_y' + str(j)] = input_q[1].split()
+				data['b_y' + str(j)] = input_q[2].split()
+				j+=1
+				out.write('%14.8s' % key)
+			out.write('\n')
+			find_x_min = 100
+			sam_dlinny = 0
+			for i in range(j):
+				if len(data['x' + str(i)])>sam_dlinny:
+					sam_dlinny = len(data['x' + str(i)])
+				for k in data['x' + str(i)]:
+					if float(k)<find_x_min:
+						find_x_min = float(k)
+			find_shag = abs(float(data['x' + str(0)][0]) - float(data['x' + str(0)][1]))
+			
+			del input_data['csrfmiddlewaretoken']
+			for i in sam_dlinny:
+				
+				for k in range(j):
+					out.write('%14.8f' % find_x_min)
+					if find_x_min==float(data['x' + str(k)][i]):
+						out.write('%14.8f' % input_data[key][i+1])
+					else:
+						out.write('%14.8f' % 0)
+				out.write('\n')
+				find_x_min+=find_shag
 
-
-
-
-		return JsonResponse(input_data)
+		return JsonResponse(message)
 	else:
 	 	return render(
 		 	request, 'general/converter.html',
