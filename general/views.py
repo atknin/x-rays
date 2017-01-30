1bd57cca077683b684f8f53488e39b1845946ce2
atknin <atknini@yahoo.com>
2017-01-30 20:43:34 +0300

    2017-01-30 17:43:34

--- general/views.py
+++ general/views.py
@@ -62,9 +62,9 @@ def converter(request):
 			j = 0
 			for key in request.POST:
 				input_q = request.POST[key].split('//')
-				 = input_q[0].split(',')
+				data['x' + str(j)] = input_q[0].split(',')
 				bot_inform.sent_to_atknin_bot('input_q: ' + str(data['x' + str(j)]), 'v')
-
+				
 				data['a_y' + str(j)] = input_q[1].split(',')
 				data['b_y' + str(j)] = input_q[2].split(',')
 				j+=1
