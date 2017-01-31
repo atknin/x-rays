7d1cf8ef6a0a70b296a21aec084c6e373d4cf179
atknin <atknini@yahoo.com>
2017-01-31 12:09:36 +0300

    2017-01-31 09:09:36

--- general/views.py
+++ general/views.py
@@ -69,8 +69,7 @@ def converter(request):
 					data['x' + str(j)] = str(input_q[0]).split(',')
 					data['y' + str(j)] = str(input_q[detector]).split(',')
 					j+=1
-					out.write('%14.8s' % key)
-					bot_inform.sent_to_atknin_bot( str( key ), 'v')
+					out.write('%14.8s' % str(key))
 
 			out.write('\n')
 			find_x_min = 100
