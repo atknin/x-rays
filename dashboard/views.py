35ac890e6d303196e90cea69cbf16b350354541d
atknin <atknini@yahoo.com>
2017-02-05 15:59:27 +0300

    2017-02-05 12:59:27

--- dashboard/views.py
+++ dashboard/views.py
@@ -16,7 +16,7 @@ def books(request):
 		path = '/home/atknin/env/xrays'+book.path
 		# repo = git.Repo( path)
 		# os.chdir(path)
-		output = subprocess.check_output(["gitbook", "build"], cwd=path)
+		output = subprocess.Popen("gitbook build", stdout=subprocess.PIPE,cwd=path).communicate() 
 		# proc = subprocess.Popen(["gitbook", "build"], stdout=subprocess.PIPE, cwd=path)
 		# output = proc.communicate()[0]
 		# proc = subprocess.check_output(['gitbook', 'build'], cwd=path)
