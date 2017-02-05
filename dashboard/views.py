2eb0af0c59f9ae2fb268b053ec8507dbc8d45907
atknin <atknini@yahoo.com>
2017-02-05 18:03:33 +0300

    2017-02-05 15:03:32

--- dashboard/views.py
+++ dashboard/views.py
@@ -15,7 +15,7 @@ def books(request):
 		book = dashboard_models.books.objects.get(pk = request.POST['id'])
 		path = '/home/atknin/env/xrays' + book.path
 
-		git_cmd = 'git submodule update --remote'
+		git_cmd = ['git', 'submodule update', '--remote']
 		kwargs = {}
 		kwargs['stdout'] = subprocess.PIPE
 		kwargs['stderr'] = subprocess.PIPE
@@ -24,7 +24,7 @@ def books(request):
 
 		proc = subprocess.Popen(shlex.split(git_cmd), **kwargs)
 		output = proc.stdout.read()
-		
+
 		# (stdout_str, stderr_str2) = proc.communicate(input=b'atknin\n')
 		# time.sleep(5)
 		# (stdout_str, stderr_str2) = proc.communicate(input=b'vfntvfnbrf43\n')
