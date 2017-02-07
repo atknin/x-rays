cbe4e515b6aa88f4714976993df95051da0c530c
atknin <atknini@yahoo.com>
2017-02-07 12:00:08 +0300

    2017-02-07 09:00:08

--- dashboard/models.py
+++ dashboard/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class books(models.Model):
 	name = models.CharField(max_length=20)
 	path = models.CharField(max_length=80)
-	url = models.CharField(max_length=80)
+	url = models.CharField(max_length=80);
 	
 	class Meta:
 		verbose_name = u'книга'
