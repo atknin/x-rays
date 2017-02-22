657ccb97d20da7193597f3ac215fa52722d48fb8
atknin <atknini@yahoo.com>
2017-02-23 00:23:22 +0300

    2017-02-22 21:23:21

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -8,5 +8,9 @@ from dashboard import models
 class books_admin(admin.ModelAdmin):
 	list_display = (u'name',u'path')
 
+class tasks_admin(admin.ModelAdmin):
+	list_display = (u'date',u'user',u'text',u'status')
+
 
 admin.site.register(models.books, books_admin)
+admin.site.register(models.tasks, tasks_admin)
--- dashboard/models.py
+++ dashboard/models.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 from django.db import models
 
-# Create your models here.
+
 class books(models.Model):
 	name = models.CharField(max_length=20)
 	path = models.CharField(max_length=80)
@@ -10,19 +10,21 @@ class books(models.Model):
 	
 	class Meta:
 		verbose_name = u'книга'
-		verbose_name_plural = u'книгы' 
+		verbose_name_plural = u'книги' 
 
 	def __unicode__(self):
 		return self.name.encode('utf-8')
 
 class tasks(models.Model):
 	text = models.CharField(max_length=1000)
-	path = models.CharField(max_length=80)
-	url = models.CharField(max_length=80)
-	
+	user = models.CharField(max_length=5)
+	feedback = models.CharField(max_length=500)
+	status = models.BooleanField(default=False)	
+	date = models.DateTimeField(auto_now_add=True, blank=True)
+
 	class Meta:
-		verbose_name = u'книга'
-		verbose_name_plural = u'книгы' 
+		verbose_name = u'Задача'
+		verbose_name_plural = u'Задачи' 
 
 	def __unicode__(self):
 		return self.name.encode('utf-8')	
