0f1c4b49314e7f17d7cb5be51992ce8a1e213830
atknin <atknini@yahoo.com>
2017-02-23 00:27:26 +0300

    2017-02-22 21:27:25

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -8,9 +8,9 @@ from dashboard import models
 class books_admin(admin.ModelAdmin):
 	list_display = (u'name',u'path')
 
-# class tasks_admin(admin.ModelAdmin):
-# 	list_display = (u'date',u'user',u'text',u'status')
+class task_admin(admin.ModelAdmin):
+	list_display = (u'date',u'user',u'text',u'status')
 
 
 admin.site.register(models.books, books_admin)
-# admin.site.register(models.tasks, tasks_admin)
+admin.site.register(models.task, task_admin)
