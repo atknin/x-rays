47af24af105e69d7d58f8f0491ff28b7375c3a36
atknin <atknini@yahoo.com>
2017-03-05 20:06:43 +0300

    2017-03-05 17:06:43

--- diffraction/views.py
+++ diffraction/views.py
@@ -87,8 +87,8 @@ def compute(request):
 				JsonResponse(output_data)
 		elif 'complited' in request.GET:
 			pc = request.GET['pc']
-			comlited = diffraction_models.list_of_calcs.objects.get(pk=request.GET['complited'])
-			bot_inform.sent_to_atknin_bot('PC: '+ comlited.status, 'v')
+			complited = diffraction_models.list_of_calcs.objects.get(pk=request.GET['complited'])
+			bot_inform.sent_to_atknin_bot('PC: '+ complited.status, 'v')
 
 			complited.status = True
 			complited.save()
