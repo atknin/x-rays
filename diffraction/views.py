e3230c7a8c51078e50f80a241bdeb96fef470045
atknin <atknini@yahoo.com>
2017-03-06 18:39:15 +0300

    2017-03-06 15:39:14

--- diffraction/views.py
+++ diffraction/views.py
@@ -78,7 +78,7 @@ def compute(request):
 
 				for i in no_calc:
 					if not i.PC is None: 
-						if i.PC.id == int(pc):
+						if i.PC.pc == int(pc):
 							output_data['status'] = 'ok'
 							output_data['JSON'] = i.JSON
 							bot_inform.sent_to_atknin_bot('PC (по запросу): '+ pc, 'v')
