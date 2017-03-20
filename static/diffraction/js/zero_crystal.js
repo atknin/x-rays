c5933dc63df0dcbc223a51a5735cda4e3ff7e010
atknin <atknini@yahoo.com>
2017-03-20 19:29:59 +0300

    2017-03-20 16:29:58

--- static/diffraction/js/zero_crystal.js
+++ static/diffraction/js/zero_crystal.js
@@ -171,13 +171,7 @@ $("#compute").click(function(){
 			alert( msg.status);
 		});
 	};
-
-
-
 });
 $("#new_compute").click(function(){
   $("#compute").prop( "disabled", false );
 });
-function AddZero(num) {
-    return (num >= 0 && num < 10) ? "0" + num : num + "";
-};
