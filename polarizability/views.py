a430926ed72ec841e603c7ca69b710f8291682a0
atknin <atknini@yahoo.com>
2017-05-11 14:53:02 +0300

    2017-05-11 11:53:02

--- polarizability/views.py
+++ polarizability/views.py
@@ -128,7 +128,7 @@ def add_crystal(request):
 def compute(request):
 	message = {}
 	message['status'] = ''
-	bot_inform.sent_to_atknin_bot(str(request.POST['crystal_id']), 'v')
+	bot_inform.sent_to_atknin_bot(str(request.POST), 'v')
 	if request.is_ajax():
 		path = os.path.realpath(os.path.dirname(sys.argv[0]))+'/polarizability/'
 		cromer_man_file = open(path+'files_for_compute/f0_CromerMann.dat')
