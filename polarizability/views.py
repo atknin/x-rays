6be830f451584aac17b5cdd06a2041fcd4f40a35
atknin <atknini@yahoo.com>
2017-05-11 18:47:01 +0300

    2017-05-11 15:47:00

--- polarizability/views.py
+++ polarizability/views.py
@@ -136,7 +136,7 @@ def compute(request):
 			mes_field = external_field.compute(request)
 			mes_normal = polarizab_funct.compute(request)
 			mes_field['difference_bragg'] = str(round((mes_normal['bragg_precize'] - mes_field['bragg_precize'])*3600,4))
-			mes_field['difference_dprmtr_percent'] = str(round(100*(mes_normal['dprmtr_precize'] - mes_field['dprmtr_precize'])/mes_normal['dprmtr_precize'],2))
+			mes_field['difference_dprmtr_percent'] = str(round(100*(mes_normal['dprmtr_precize'] - mes_field['dprmtr_precize'])/mes_normal['dprmtr_precize'],4))
 			return JsonResponse(mes_field)
 		else:
 			return JsonResponse(polarizab_funct.compute(request))
