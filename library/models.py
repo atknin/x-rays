fd6f575ee08381aea9bca3f14239a004de0415ba
atknin <atknini@yahoo.com>
2017-06-04 17:12:22 +0300

    2017-06-04 14:12:21

--- library/models.py
+++ library/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Tag(models.Model):
     text = models.CharField(max_length=64, unique=True)
     def __str__(self):
-        return 'tag {id}. {text}]'.format(
+        return 'tag {id}. {text}'.format(
             id=self.id, text=self.text)
     class Meta:
         verbose_name = u'tag'
@@ -15,8 +15,8 @@ class author(models.Model):
     last_name_rus = models.CharField(max_length=40)
 
     def __str__(self):
-        return 'auth {id}. {text}]'.format(
-            id=self.id, text=self.last_name_rus.encode('utf-8'))
+        return 'auth {id}. {text}'.format(
+            id=self.id, text=self.last_name_rus)
 
     class Meta:
         verbose_name = u'автор'
@@ -34,7 +34,7 @@ class labrary(models.Model):
 
     def __str__(self):
         return 'id. {name}]'.format(
-            id=self.id, name=self.title.encode('utf-8'))
+            id=self.id, name=self.title)
     class Meta:
         verbose_name = u'Литературу'
         verbose_name_plural = u'Литература'
