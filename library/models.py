e5f56df36fc17ae7de27cf65dc9fe300de6c9488
atknin <atknini@yahoo.com>
2017-06-04 16:34:16 +0300

    2017-06-04 13:34:16

--- library/models.py
+++ library/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Tag(models.Model):
 
     text = models.CharField(max_length=64, unique=True)
-    
+
     def __str__(self):
         return 'Tag[id: {id}, text: {text}]'.format(
             id=self.id, text=self.text)
