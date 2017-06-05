d9ed93bc22b013d5ae6c2a53df85fdaa110839a4
atknin <atknini@yahoo.com>
2017-06-05 17:59:10 +0300

    2017-06-05 14:59:10

--- library/views.py
+++ library/views.py
@@ -4,7 +4,7 @@ from library import models as library_models
 # Create your views here.
 def index(request):
     arg = {}
-    arg['library'] = library_models.labrary.objects.all()[::-1]
+    arg['library'] = library_models.labrary.objects.all()#[::-1]
     return render( request, 'library/index.html', arg )
 
 def add(request):
