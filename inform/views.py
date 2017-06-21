aabccacded0377779c7fe236e413b1097d3c2fbd
atknin <atknini@yahoo.com>
2017-06-21 15:50:26 +0300

    2017-06-21 12:50:26

--- inform/views.py
+++ inform/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from inform import models as inform_models
 # Create your views here.
 def index(request):
     if request.is_ajax():
-        Name = request.POST['Name']
-        LastName = request.POST['LastName']
-        email = request.POST['email']
-        phone = request.POST['phone']
+        out_data = {}
+        try:
+            participant = inform_models.participants.objects.create(Name = request.POST['Name'],
+                                                                    LastName = request.POST['LastName'],
+                                                                    email = request.POST['email'],
+                                                                    phone = request.POST['phone'])
+            out_data['status'] = 'успешно'
+            return JsonResponse(out_data)
+        except Exception as e:
+            out_data['status'] = 'безуспешно'
+            return JsonResponse(out_data)
+
     else:
      	return render(
     	 	request, 'inform/index.html',
