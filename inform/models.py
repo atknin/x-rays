6f7736f18b6c96c1faf8f56e0fe3529c0b994af8
atknin <atknini@yahoo.com>
2017-07-14 12:34:45 +0300

    2017-07-14 09:34:45

--- inform/models.py
+++ inform/models.py
@@ -29,10 +29,15 @@ class question_choose(models.Model):
 	def __str__(self):
 		return self.text
 
+class types_for_question(models.Model):
+	text = models.CharField(max_length=300)
+	def __str__(self):
+		return self.text
 
 class questions(models.Model):
 	text = models.CharField(max_length=300)
-	choose = models.ManyToManyField(question_choose,blank=True)
+	types = models.ForeignKey(types_for_question)
+	choose = models.ManyToManyField(question_choose, blank=True)
 	DateTime = models.DateTimeField(auto_now_add=True, blank=True)
 	class Meta:
 		verbose_name = u'Вопрос'
