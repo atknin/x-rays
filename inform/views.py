ee02f3729b4946e6d2a4508554af503cf4bb8f4e
atknin <atknini@yahoo.com>
2017-07-14 18:05:36 +0300

    2017-07-14 15:05:36

--- inform/views.py
+++ inform/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from inform import models as inform_models
+import datetime
 # Create your views here.
 def index(request):
     if request.is_ajax():
@@ -10,6 +11,11 @@ def index(request):
                                                                     LastName = request.POST['LastName'],
                                                                     email = request.POST['email'],
                                                                     phone = request.POST['phone'])
+            try:
+                participant.ip = request.META['REMOTE_ADDR']
+                participant.save()
+            except Exception as e:
+                participant.save()
             out_data['status'] = 'успешно'
             return JsonResponse(out_data)
         except Exception as e:
@@ -20,3 +26,10 @@ def index(request):
      	return render(
     	 	request, 'inform/index.html',
     	 	)
+def questions(request):
+    argv = {}
+    yesterday = datetime.date.today() - datetime.timedelta(days=1)
+    argv['questions'] = inform_models.questions.objects.objects.filter(DateTime__gt=yesterday)
+    return render(
+        request, 'inform/questions.html',argv
+        )
