bb5d44f214298bca9a42f65e33fcb4440097803f
atknin <atknini@yahoo.com>
2017-07-15 19:12:48 +0300

    2017-07-15 16:12:48

--- inform/views.py
+++ inform/views.py
@@ -83,10 +83,11 @@ def questions_results(request):
                 argv['results'][i.questions.id]['result'] = i.number
                 argv['results'][i.questions.id]['N'] = 1
         else:
-            if i.questions.id in argv['results']:
-                argv['results'][i.questions.id] += 1
+            if i.question_choose.id in argv['results'][i.questions.id]:
+                argv['results'][i.questions.id][i.question_choose.id] += 1
             else:
-                argv['results'][i.questions.id] = 1
+                argv['results'][i.questions.id] = {}
+                argv['results'][i.questions.id][i.question_choose.id] = 1
 
     bot_inform.sent_to_atknin_bot(str(argv['results']), 'v') # проинформируем в telegramm bot
     return render(
