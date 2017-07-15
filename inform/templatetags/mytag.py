cbaf7b98c561cd5bfacbc73f67b7402d65ab5dbc
atknin <atknini@yahoo.com>
2017-07-15 21:25:06 +0300

    2017-07-15 18:25:05

--- inform/templatetags/mytag.py
+++ inform/templatetags/mytag.py
@@ -2,5 +2,8 @@ from django import template
 register = template.Library()
 
 @register.filter
-def get_item(dictionary, key):
-    return dictionary.get(key)
+def div(value, arg):
+    try:
+        return int(value) / int(arg)
+    except (ValueError, ZeroDivisionError):
+        return None
